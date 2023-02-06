10c1eb72bb1f302dbc1df983419d5317a697204a
Corow <90448778+CorentinDenoulet@users.noreply.github.com>
2023-02-06 16:42:32 +0100

    HttpInterceptor function
    Coding HttpInterceptor function

--- pw/pw-jwt-oauth/client/src/app/services/auth/auth-jwt.interceptor.ts
+++ pw/pw-jwt-oauth/client/src/app/services/auth/auth-jwt.interceptor.ts
@@ -25,15 +25,17 @@ export class JwtInterceptor implements HttpInterceptor {
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     //  retrieve jwt token from client storage (local or session) with the key 'authenticationToken'
-    // let token = this.ng2localStorage.retrieve(....) || this.ng2sessionStorage.retrieve(....);
+    let token = this.ng2localStorage.retrieve('authenticationToken') || this.ng2sessionStorage.retrieve('authenticationToken');
+
     //  verify token is present
-    // if (...){
-    //  set authorization header in the request with the token : 'Authorization: Bearer __token__'
-    // req = req.clone({
-    //   setHeaders: {
-    //     Authorization: ....
-    //   }
-    // });
+    if (token) {
+      //  set authorization header in the request with the token : 'Authorization: Bearer __token__'
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+    }
 
     return next.handle(req).pipe(
       tap(
