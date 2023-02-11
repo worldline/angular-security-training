58389fb915762cc3fd26214971fc9b416ebca66b
imm8 <109025614+imm8@users.noreply.github.com>
2023-02-11 16:45:06 +0100

    Fixed CSPResource class
    Removed an additional closing bracket.

--- pw/pw-csp-nonce/server/src/main/java/com/worldline/bookstore/web/rest/CSPResource.java
+++ pw/pw-csp-nonce/server/src/main/java/com/worldline/bookstore/web/rest/CSPResource.java
@@ -29,60 +29,59 @@ public class CSPResource {
     /** Used for Script Nonce */
     private SecureRandom prng = null;
         
-        @GetMapping("/csp")
-        @Timed
-        // Add Script Nonce CSP Policy
-        public ResponseEntity<?> generateCSP(HttpServletResponse response) {
-            // --Get its digest
-            MessageDigest sha;
-            // --Generate a random number
-            String randomNum;
-            try {
-                this.prng = SecureRandom.getInstance("SHA1PRNG");
-                randomNum = new Integer(this.prng.nextInt()).toString();
-                sha = MessageDigest.getInstance("SHA-1");
-            }
-            catch (NoSuchAlgorithmException e) {
-                return new ResponseEntity<>(Collections.singletonMap("CSPException",e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-            
-            byte[] digest = sha.digest(randomNum.getBytes());
-            
-            // --Encode it into HEXA
-            char[] scriptNonce = Hex.encode(digest);
-            
-            String csp = "script-src" +
-                " 'unsafe-eval' 'strict-dynamic' " +
-                " 'nonce-"+String.valueOf(scriptNonce)+"'" +
-                " 'sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4='" + // SRI hashes for https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js (work only for Chrome)
-                ";" +
-                // add connect-src directive to adapt CSP over cross-origin requests (CORS)  
-                "connect-src"+
-                " http://localhost:8080 http://localhost:4200 ws://localhost:4200"
-                + ";"+
-                " style-src" +
-                " 'self' 'unsafe-inline'"+
-                ";" +
-                " font-src" +
-                " 'self' "+
-                ";" +
-                " img-src" +
-                " 'self' data:" +
-                ";" +
-                " child-src" +
-                " 'self' " +
-                ";" +
-                " object-src" +
-                " 'none' " +
-                ";" +
-                " default-src" +
-                " 'self' ";
-            
-              CSP conf = new CSP(csp);
-              conf.setNonce(String.valueOf(scriptNonce));
-              
-              log.debug(conf.toString());
-              return ResponseEntity.ok(conf);  
+    @GetMapping("/csp")
+    @Timed
+    // Add Script Nonce CSP Policy
+    public ResponseEntity<?> generateCSP(HttpServletResponse response) {
+        // --Get its digest
+        MessageDigest sha;
+        // --Generate a random number
+        String randomNum;
+        try {
+            this.prng = SecureRandom.getInstance("SHA1PRNG");
+            randomNum = new Integer(this.prng.nextInt()).toString();
+            sha = MessageDigest.getInstance("SHA-1");
         }
+        catch (NoSuchAlgorithmException e) {
+            return new ResponseEntity<>(Collections.singletonMap("CSPException",e.getLocalizedMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+            
+        byte[] digest = sha.digest(randomNum.getBytes());
+            
+        // --Encode it into HEXA
+        char[] scriptNonce = Hex.encode(digest);
+            
+        String csp = "script-src" +
+            " 'unsafe-eval' 'strict-dynamic' " +
+            " 'nonce-"+String.valueOf(scriptNonce)+"'" +
+            " 'sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4='" + // SRI hashes for https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js (work only for Chrome)
+            ";" +
+            // add connect-src directive to adapt CSP over cross-origin requests (CORS)  
+            "connect-src"+
+            " http://localhost:8080 http://localhost:4200 ws://localhost:4200"
+            + ";"+
+            " style-src" +
+            " 'self' 'unsafe-inline'"+
+            ";" +
+            " font-src" +
+            " 'self' "+
+            ";" +
+            " img-src" +
+            " 'self' data:" +
+            ";" +
+            " child-src" +
+            " 'self' " +
+            ";" +
+            " object-src" +
+            " 'none' " +
+            ";" +
+            " default-src" +
+            " 'self' ";
+            
+            CSP conf = new CSP(csp);
+            conf.setNonce(String.valueOf(scriptNonce));
+              
+            log.debug(conf.toString());
+            return ResponseEntity.ok(conf);  
     }
 }
